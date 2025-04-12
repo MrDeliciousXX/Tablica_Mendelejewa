37e674c088c52a4bf66738f5132e60cfbaa9a718
MrDeliciousXX <lp303249@student.polsl.pl>
2025-04-12 14:30:21 +0200

    klikanie i powiększanie

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ document.querySelectorAll('.element').forEach(item => {
       document.getElementById('element-animation').src = gifPath;
     });
   });
+
+// Pobieramy wszystkie komórki tabeli
+const cells = document.querySelectorAll('#periodic-table td');
+
+// Funkcja obsługująca kliknięcie
+cells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    // Usuwamy klasę "selected" z wszystkich komórek
+    cells.forEach(c => c.classList.remove('selected'));
+    
+    // Dodajemy klasę "selected" do klikniętej komórki
+    cell.classList.add('selected');
+  });
+});
+
+// Pobranie elementów
+const modal = document.getElementById("myModal");
+const img = document.getElementById("element-animation");
+const modalImg = document.getElementById("img01");
+const span = document.getElementsByClassName("close")[0];
+
+// Po kliknięciu na miniaturkę, obrazek zostaje wyświetlony w modalnym oknie
+img.onclick = function() {
+  modal.style.display = "block";
+  modalImg.src = this.src; // Ustawiamy źródło powiększonego obrazu
+}
+
+// Po kliknięciu w "X", zamknięcie modala
+span.onclick = function() {
+  modal.style.display = "none";
+}
